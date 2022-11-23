b049ffb96a7c5c98c0d3b3b98e943679feb8586c
Jal-Fazer <tishinaliza@gmail.com>
2022-11-23 17:51:19 +0200

    HomeWork6 added. Hope, i get the task right

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,60 @@
+﻿//ДЗ. 6.
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+/*
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i+1} number: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) 
+        if(i < (array.Length - 1)) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]} -> ");
+}
+
+int CountPositive(int[] array)
+{
+    int num = 0;
+    for(int i = 0; i < array.Length; i++)
+        if (array[i] > 0) num++;
+    return num;
+}
+
+Console.Write("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] myarray = CreateArray(m);
+ShowArray(myarray);
+Console.Write(CountPositive(myarray));
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = x * k1 + b1;
+    Console.WriteLine($"{y}");
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+
+Console.Write("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Intersection(b1, k1, b2, k2);
